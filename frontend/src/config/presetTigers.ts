0a66895824e7478443e2ba43cbbcdad51f053266
Andrew Ayson <drewubaldeayson@gmail.com>
2021-12-26 14:11:06 +0800

    [UPDATE] - preset tigers

--- frontend/src/config/presetTigers.ts
+++ frontend/src/config/presetTigers.ts
@@ -3,9 +3,9 @@ export const TigerPreset = {
     UnAuthorizedTiger: "0046003274779473",
     CuteTiger: "2038444364911085",
     CoolTiger: "1561253421617463",
-    FancyTiger: "2900397930256857",
-    SpaceTiger: "9520612801289657",
-    GirlyTiger: "3908550096369189",
-    WeirdTiger: "5688959649746782",
+    FancyTiger: "2900397930252253",
+    SpaceTiger: "9520612801285353",
+    GirlyTiger: "3908550096365163",
+    WeirdTiger: "5688959649745262",
   };
   
